ad9918cec9156d2457c99f71968c1ad3b09c0631
alex <alexandru.cavaler2003@gmail.com>
2025-04-08 18:05:06 +0300

    Update RestApiFactoryV2.java

--- IdeaProjects/sem 4/src/ro/ase/acs/simplefactory/RestApiFactoryV2.java
+++ IdeaProjects/sem 4/src/ro/ase/acs/simplefactory/RestApiFactoryV2.java
@@ -3,9 +3,9 @@ package ro.ase.acs.simplefactory;
 public class RestApiFactoryV2 {
     public RestApi getRestApi(ServerType serverType){
         switch (serverType){
-                case DEV -> { return new RestApiDevelopment(); }
-                case PROD -> { return new RestApiProduction(); }
-                default -> throw new IllegalArgumentException();
+            case DEV -> { return new RestApiDevelopment(); }
+            case PROD -> { return new RestApiProduction(); }
+            default -> throw new IllegalArgumentException();
         }
     }
 }
