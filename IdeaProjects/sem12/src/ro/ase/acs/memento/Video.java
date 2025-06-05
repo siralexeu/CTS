8ad37a27df4bb4c68d9c6521170a63aaf86e82ce
alex <alexandru.cavaler2003@gmail.com>
2025-06-06 00:54:00 +0300

    Update Video.java

--- IdeaProjects/sem12/src/ro/ase/acs/memento/Video.java
+++ IdeaProjects/sem12/src/ro/ase/acs/memento/Video.java
@@ -1,4 +1,43 @@
 package ro.ase.acs.memento;
 
-public class Video {
+public class Video implements Cloneable {
+    private String title;
+    private int length;
+
+    public Video(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Video copy = (Video) super.clone();
+        copy.title = title;
+        copy.length = length;
+        return copy;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "title='" + title + '\'' +
+                ", length=" + length +
+                '}';
+    }
 }
