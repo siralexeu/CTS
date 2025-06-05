c58f654af1058eb4ee9babc1bddc72f90c631753
alex <alexandru.cavaler2003@gmail.com>
2025-06-06 00:55:42 +0300

    Update VideoEditor.java

--- IdeaProjects/sem12/src/ro/ase/acs/memento/VideoEditor.java
+++ IdeaProjects/sem12/src/ro/ase/acs/memento/VideoEditor.java
@@ -1,4 +1,30 @@
 package ro.ase.acs.memento;
 
 public class VideoEditor {
+    private Video video;
+    private HistoryManager historyManager;
+
+    public VideoEditor() {
+        video = new Video("Untitled", 0);
+        historyManager = new HistoryManager();
+    }
+
+    public void edit(int minutes) {
+        video.setLength(video.getLength() + minutes);
+    }
+
+    public void save() {
+        historyManager.addToHistory(video);
+    }
+
+    public void undo() {
+        video = historyManager.getFromHistory();
+    }
+
+    @Override
+    public String toString() {
+        return "VideoEditor{" +
+                "video=" + video +
+                '}';
+    }
 }
